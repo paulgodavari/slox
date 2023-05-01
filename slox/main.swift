d91b7ab5bb81705b1ca3e11bbf710f6f5cd4b3df
Paul Godavari <paulgod@gmail.com>
2023-05-01 07:56:31 -0500

    File order
    - Delete commented code

--- slox/main.swift
+++ slox/main.swift
@@ -9,18 +9,6 @@ import Foundation
 import DebugLog
 
 
-//func addThings(_ p: Int...) -> Int {
-//    var sum = 0
-//    for i in p {
-//        sum += i
-//        print("i=\(i), sum=\(sum)")
-//    }
-//    return sum
-//}
-//print("sum: \(addThings(1, 2, 3, 4, 5, 6))")
-//print("sum: \(addThings(10, 20, 30, 40, 50))")
-
-
 Logger.log("slox starting")
 
 if CommandLine.arguments.count > 2 {
